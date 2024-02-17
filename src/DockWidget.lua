924e83110a974246fac39423ffd24bb5af9bfddc
ashi <40967034+frqstbite@users.noreply.github.com>
2024-02-17 17:30:47 -0500

    Corrected definition of DockWidget

--- src/DockWidget.lua
+++ src/DockWidget.lua
@@ -1,36 +1,37 @@
-local root = script.Parent.Parent
-local Fusion = require(root.Packages.Fusion)
-local Hydrate = Fusion.Hydrate
-local unwrap = require(root.unwrap)
+local root = script.Parent
 
-type DockWidgetProps = {
-    
-}
+return function(fusion, plugin)
+    local Hydrate = fusion.Hydrate
+    local unwrap = require(root.unwrap)
 
-local function DockWidget(id: string)
-    return function(props: {[string]: any})
-
-        props = props or {}
-        local plugin = unwrap(props.Plugin) :: Plugin
-        local size = unwrap(props.Size) :: Vector2
-        local minimumSize = unwrap(props.MinimumSize) :: Vector2?
+    type DockWidgetProps = {
         
-        -- Create widget
-        local widget = plugin:CreateDockWidgetPluginGui(id, DockWidgetPluginGuiInfo.new(
-            Enum.InitialDockState.Float,
-            false,
-            false,
-            size.X,
-            size.Y,
-            if minimumSize then minimumSize.X else size.X,
-            if minimumSize then minimumSize.Y else size.Y
-        ))
-        
-        props.Name = id
-        props.Size = nil
-        props.MinimumSize = nil
-        return Hydrate(widget)(props)
+    }
+
+    local function DockWidget(id: string)
+        return function(props: {[string]: any})
+
+            props = props or {}
+            local size = unwrap(props.Size) :: Vector2
+            local minimumSize = unwrap(props.MinimumSize) :: Vector2?
+            
+            -- Create widget
+            local widget = plugin:CreateDockWidgetPluginGui(id, DockWidgetPluginGuiInfo.new(
+                Enum.InitialDockState.Float,
+                false,
+                false,
+                size.X,
+                size.Y,
+                if minimumSize then minimumSize.X else size.X,
+                if minimumSize then minimumSize.Y else size.Y
+            ))
+            
+            props.Name = id
+            props.Size = nil
+            props.MinimumSize = nil
+            return Hydrate(widget)(props)
+        end
     end
-end
 
-return DockWidget
+    return DockWidget
+end
