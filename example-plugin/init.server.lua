08880a0085c7b4a3844100a7dd2b052807bf8300
ashi <40967034+frqstbite@users.noreply.github.com>
2024-03-18 18:19:11 -0400

    Finished example plugin

--- example-plugin/init.server.lua
+++ example-plugin/init.server.lua
@@ -9,27 +9,58 @@ local Value = Fusion.Value
 
 local FusionPlugin = require(packages.FusionPlugin)(Fusion, plugin)
 local DockWidget = FusionPlugin.DockWidget
+local Action = FusionPlugin.Action
 local Toolbar = FusionPlugin.Toolbar
 local ToolbarButton = FusionPlugin.ToolbarButton
 
 local image = "rbxassetid://7120897383"
-local open = Value(false)
+local open = Value(false) --whetyher the settings widget is open
 
+-- Plugin menu
+-- Nothing special here yet :(
+local coolmenu = plugin:CreatePluginMenu("coolmenu", "Cool Menu", "")
+coolmenu:AddAction(Action "coolaction" {
+    Bindable = true,
+    Icon = image,
+    Text = "Cool Action",
+    Tooltip = "this means only good things",
+
+    [OnEvent "Triggered"] = function()
+        print("cool things underway.......")
+    end,
+})
+
+-- Set up toolbar
 Toolbar "Example Plugin" {
+    -- Cool button
+    ToolbarButton "cool menu" {
+        Active = false,
+        [OnEvent "Click"] = function()
+            local r = coolmenu:ShowAsync()
+            print(r and r.Name)
+        end,
+        
+        ClickableWhenViewportHidden = true,
+        Text = "Cool",
+        Tooltip = "Open example plugin's cool menu",
+        Icon = image,
+    },
+
+    -- Settings button
     ToolbarButton "settings" {
         Active = open,
-        ClickableWhenViewportHidden = true,
         [OnEvent "Click"] = function()
             open:set(not open:get())
         end,
-
-        -- NON-REACTIVE
+        
+        ClickableWhenViewportHidden = true,
         Text = "Settings",
         Tooltip = "Open example plugin's settings",
         Icon = image,
     },
 }
 
+-- Settings widget
 DockWidget ("examplePluginSettings") {
     Title = "Example Plugin Settings",
     Enabled = open,
