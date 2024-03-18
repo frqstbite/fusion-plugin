79e58a2c637638311c5d3e97ffbe41f8f541bc69
ashi <40967034+frqstbite@users.noreply.github.com>
2024-03-18 19:46:11 -0400

    Instantiated module now falls back on the associated Plugin object

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,10 @@
 return function(fusion: any, plugin: Plugin)
-    return {
+    return setmetatable({
         Action = require(script.Action)(fusion, plugin),
         DockWidget = require(script.DockWidget)(fusion, plugin),
         Toolbar = require(script.Toolbar)(fusion, plugin),
         ToolbarButton = require(script.ToolbarButton)(fusion, plugin),
-    }
+    }, {
+        __index = plugin, --For convenience
+    })
 end
